cf393f54aa33ea93ce139e538dd1b6df06206010
Your Name <you@example.com>
2020-11-06 10:59:42 -0400

    Melhorando Campo de Busca

---
+++ src/AppStyled.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    display: flex;
+    background-color: #FF0000;
+    height: 100vh;
+`;
+
+export const Menu = styled.div`
+    display: flex;
+    background-color: #136713;
+    width: 80px;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const PageBody = styled.div`
+    display: flex;
+    background-color: #00980d;
+    background-image: url('/assets/bg.png');    
+    flex: 1;
+`;
